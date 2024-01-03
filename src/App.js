c484f553872b6e32ef572bf0c8e939db0b4a381f
wolterbwambale <benplaan1@gmail.com>
2024-01-03 23:08:51 +0300

    add components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import Navbar from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Navbar />
+        <Home />
         <h1>Hello world</h1>
       </header>
     </div>
