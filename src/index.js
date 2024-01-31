c4000eb67b4ebb3987965571a0a51670a769f90c
Bwambale Benny Wolter <benplaan1@gmail.com>
2024-02-01 01:11:13 +0300

    update

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import '@fontsource/outfit';
+import '@fontsource/roboto';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
